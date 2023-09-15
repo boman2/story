ae6ceb9574132a84520958edb260628e0e5270e3
boman kim <121792700+boman2@users.noreply.github.com>
2023-09-15 23:26:34 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,8 +26,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 2.0,
-  defaultPageCover: 2.0,
+  defaultPageIcon: true,
+  defaultPageCover: 'null',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
